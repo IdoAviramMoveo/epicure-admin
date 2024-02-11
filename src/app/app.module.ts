7cecd28cb1219c3fb01848fcd0eb812c2aa08ded
IdoAviramMoveo <idoa@moveo.co.il>
2024-02-11 15:21:01 +0200

    Initialized the structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { RestaurantsTableComponent } from './components/restaurants-table/restaurants-table.component';
+import { ChefsTableComponent } from './components/chefs-table/chefs-table.component';
+import { DishesTableComponent } from './components/dishes-table/dishes-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantsTableComponent,
+    ChefsTableComponent,
+    DishesTableComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule, MatTableModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/restaurants-table/restaurants-table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurants-table',
+  templateUrl: './restaurants-table.component.html',
+  styleUrl: './restaurants-table.component.scss',
+})
+export class RestaurantsTableComponent {}
