20e695ed4793183fc10395f659f4b2141aaf27ee
IdoAviramMoveo <idoa@moveo.co.il>
2024-03-03 10:23:35 +0200

    Edited the login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -56,6 +57,7 @@ import { ImageDialogComponent } from './components/image-dialog/image-dialog.com
     MatSelectModule,
     MatProgressSpinnerModule,
     BrowserAnimationsModule,
+    MatTooltipModule,
   ],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AuthService } from '../../services/auth.service';
 
@@ -11,23 +12,51 @@ import { AuthService } from '../../services/auth.service';
   encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent implements OnInit {
-  email: string = '';
-  password: string = '';
+  loginForm: FormGroup;
+  emailInvalid: boolean = false;
 
   constructor(
     private authService: AuthService,
     private router: Router,
-    private snackBar: MatSnackBar
-  ) {}
+    private snackBar: MatSnackBar,
+    private formBuilder: FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(/^.+@.+\..+$/),
+        ],
+      ],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit() {
     if (this.authService.isLoggedIn()) {
       this.router.navigate(['/']);
     }
+    this.loginForm.get('email').statusChanges.subscribe((status) => {
+      this.emailInvalid =
+        status === 'INVALID' && this.loginForm.get('email').touched;
+    });
+  }
+
+  emailBlurHandler() {
+    this.emailInvalid =
+      this.loginForm.get('email').touched &&
+      this.loginForm.get('email').invalid;
   }
 
   onSubmit() {
-    this.authService.adminLogin(this.email, this.password).subscribe({
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const { email, password } = this.loginForm.value;
+    this.authService.adminLogin(email, password).subscribe({
       next: (res) => {
         sessionStorage.setItem('token', res.token);
         this.router.navigate(['/restaurants']);
